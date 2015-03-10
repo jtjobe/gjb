426f66cad7dc6878b5fbe3b01f7b0ed1d7cae22d
JT <jtjobe@gmail.com>
2015-03-10 16:58:22 -0400

    Finish styling of nav bar (top of page + sticky style) for all browser widths

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,23 @@
+$(window).scroll(function() {
+if ($(this).scrollTop() > 10){  
+    $('.full-nav').addClass('full-nav-sticky');
+    $('.logo-col').addClass('logo-col-sticky');
+    $('.logo').addClass('logo-sticky');
+    $('.logo-container').addClass('logo-container-sticky');
+    $('.nav-col').addClass('nav-col-sticky');
+    $('.nav-link').addClass('nav-link-sticky')
+    $('.home-link').addClass('home-link-sticky');
+  }
+  else{
+    $('.full-nav').removeClass('full-nav-sticky');
+    $('.logo-col').removeClass('logo-col-sticky');
+    $('.logo').removeClass('logo-sticky');
+    $('.logo-container').removeClass('logo-container-sticky');
+    $('.nav-col').removeClass('nav-col-sticky');
+    $('.nav-link').removeClass('nav-link-sticky')
+    $('.home-link').removeClass('home-link-sticky');
+  }
+});
+
+
 
